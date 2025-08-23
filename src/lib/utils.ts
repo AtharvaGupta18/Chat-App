cd843c0dd1d8a04fe7c668b6bb56180b0c7d4c9d
Deepti Gupta <deepti.gpt28@gmail.com>
2025-08-23 19:43:28 +0000

    use initial of first name and second name

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,10 +6,14 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getInitials(name: string) {
-  const names = name.trim().split(' ');
-  if (names.length === 1 && names[0] !== '') {
-    return names[0].substring(0, 2).toUpperCase();
+  const names = name.trim().split(' ').filter(n => n);
+  if (names.length === 0) {
+    return "";
   }
-  const initials = names.map((n) => n[0]).join('');
-  return (initials.length > 2 ? initials.substring(0, 2) : initials).toUpperCase();
+  if (names.length === 1) {
+    return names[0][0].toUpperCase();
+  }
+  const firstNameInitial = names[0][0];
+  const lastNameInitial = names[names.length - 1][0];
+  return `${firstNameInitial}${lastNameInitial}`.toUpperCase();
 }
