cb8b7ddd072067eeeb8b473441f0531fdf1b2002
Deepti Gupta <deepti.gpt28@gmail.com>
2025-08-23 19:19:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/chat/chat-window.tsx
+++ src/components/chat/chat-window.tsx
@@ -14,6 +14,7 @@ import {
   getDoc,
   writeBatch,
   increment,
+  updateDoc,
 } from "firebase/firestore";
 import { SendHorizonal, User as UserIcon } from "lucide-react";
 import { firestore } from "@/lib/firebase";
