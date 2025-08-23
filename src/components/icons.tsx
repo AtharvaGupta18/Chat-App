1fd3982a2c44a1c0db20255b5c4347606caf134f
Deepti Gupta <deepti.gpt28@gmail.com>
2025-08-23 20:07:48 +0000

    use ion icon for the logo <ion-icon name="chatboxes"></ion-icon>

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,17 +5,18 @@ import type { SVGProps } from 'react';
 export function GlimpseLogo({ className, ...props }: SVGProps<SVGSVGElement>) {
   return (
     <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className={cn("h-6 w-6", className)}
-        {...props}
-        >
-        <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={cn("h-6 w-6", className)}
+      {...props}
+    >
+      <path d="M14 9a2 2 0 0 1-2 2H6l-4 4V5a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v4Z" />
+      <path d="M18 9h2a2 2 0 0 1 2 2v10l-4-4h-6a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h2Z" />
     </svg>
   );
 }
