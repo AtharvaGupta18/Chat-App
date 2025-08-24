81d85eb594b4099a7f35601fd73ac8bb819be4ae
Atharva Gupta <70589193+AtharvaGupta18@users.noreply.github.com>
2025-08-24 20:28:06 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCBMs3TSLkc6x-i1rvGOlUoDDrkgrTZ8L4",
+  authDomain: "whisperlink-ys32m.firebaseapp.com",
+  projectId: "whisperlink-ys32m",
+  storageBucket: "whisperlink-ys32m.firebasestorage.app",
+  messagingSenderId: "194543347950",
+  appId: "1:194543347950:web:386b34219b93209a2c2bff"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
